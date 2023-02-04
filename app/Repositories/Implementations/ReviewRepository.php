cd4de56c2c982866d33c5c692ffbf27c98727f45
dang xuan vinh <vinh.dang23@student.passerellesnumeriques.org>
2023-02-04 15:05:41 +0700

    fix review repository

---
+++ app/Repositories/Implementations/ReviewRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories\Implementations;
+
+use App\Models\Review;
+use Illuminate\Support\Facades\DB;
+class ReviewRepository extends BaseRepository implements \App\Repositories\Interfaces\IReviewRepository
+{
+
+    public function getModel(): string
+    {
+        return Review::class;
+    }
+    public function getAllReview($pitch_id): mixed
+    {
+        return  DB::table('reviews')
+        ->select('users.id as user_id','users.fullname','users.avatar','pitchs.namepitch','reviews.title')
+        ->join('pitchs','pitchs.id','=','reviews.pitch_id')
+        ->join('users','users.id','=','reviews.user_id')
+        ->where('reviews.pitch_id',$pitch_id)
+        ->get();
+    }
+}
