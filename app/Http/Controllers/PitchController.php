e2a0d613d1532a87269862a88d83f689e9b5ec7b
tamxin202 <tamdinh793@gmail.com>
2023-02-14 16:54:27 +0700

    create update and delete pitch in controller

--- app/Http/Controllers/PitchController.php
+++ app/Http/Controllers/PitchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pitch;
 use App\Services\Interfaces\IPitchService;
 use Illuminate\Http\Request;
 
@@ -39,4 +40,66 @@ class PitchController extends Controller
             ]
         );
     }
+
+//Admin
+public function showPitchs(){
+    $pitchs = $this->pitchService->getAllPitchs();
+    return view('pages.pitchs.index',compact('pitchs'));
+}
+public function getAddForm(){
+    return view('pages.pitchs.addPitch');
+}
+public function postAddForm(Request $request)
+    {
+        $pitch = new Pitch();
+        $pitch->namepitch = $request->input("inputName");
+        $pitch->address = $request->input("inputAddress");
+        $pitch->phonenumber = $request->input("inputPnum");
+        $pitch->price_hour = $request->input("inputPrice");
+        if($request->hasFile("inputImage")){
+            $destinationPath = 'public/images/pitchs';
+            $image = $request->file('inputImage');
+            $image_name = $image->getClientOriginalName();
+            $path = $request->file('inputImage')->storeAs($destinationPath,$image_name);
+            $data["image"] = 'storage/images/pitchs/'.$image_name;
+            $pitch->image = $data["image"];
+        }
+        $pitch->save();
+
+        return redirect()->route('pitchs.index')->with('success','Create Successfully');
+}
+
+public function updateForm(Request $request,$id){
+    $pitch = Pitch::find($id);
+
+    $pitch->namepitch = $request->input("editName");
+    $pitch->address = $request->input("editAddress");
+    $pitch->phonenumber = $request->input("editPnum");
+    $pitch->price_hour = $request->input("editPrice");
+    if($request->hasFile("editImage")){
+        $destinationPath = 'public/images/pitchs';
+        $image = $request->file('editImage');
+        $image_name = $image->getClientOriginalName();
+        $path = $request->file('editImage')->storeAs($destinationPath,$image_name);
+        $data["image"] = 'storage/images/pitchs/'.$image_name;
+        $pitch->image = $data["image"];
+    }
+    $pitch->update();
+    return redirect()->route('pitchs.index')->with('success','Updated Successfully');
+}
+
+public function editPitch($id){
+
+
+        $pitch = Pitch::find($id);
+        return view("pages.pitchs.editPitch",compact('pitch'));
+    }
+
+public function deletePitchs($id){
+    $pitch = Pitch::find($id);
+    $pitch->delete($id);
+    // $this->pitchService->deletePitchs($id);
+        //echo ("Delete successfully");
+    return redirect()->route('pitchs.index')->with('success','Delete Successfully');
+}
 }
