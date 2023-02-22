c32871dc50a31535db9ebf83f9d3e62636805cec
tamxin202 <tamdinh793@gmail.com>
2023-02-22 08:38:50 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\PitchController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,13 +22,16 @@ use App\Http\Controllers\PostController;
 Route::get('home', function () {
     return view('layout.main');
 })->name("home.index");;
+Route::get('dashboard', function () {
+    return view('pages.index');
+})->name("dashboard");;
 Route::prefix("/custommer")->group(function () {
     Route::get("", [UserController::class, "getAllCustommer"])->name("customers.index");;
 });
 Route::prefix("pitch")->group(function () {
     Route::get("/", [PitchController::class, "showPitchs"])->name("pitchs.index");
     Route::post('/delete/{id}', [PitchController::class, "deletePitchs"])->name("pitchs.deletePitchs");
-    Route::get("/add", [PitchController::class, "getAddForm"])->name("getAddForm");
+    Route::get('/add', [PitchController::class, "getAddForm"])->name("getAddForm");
     Route::post('/addForm',[PitchController::class,"postAddForm"])->name("postAddForm");
     Route::post('/edit/{id}', [PitchController::class, "editPitch"])->name("pitchs.editPitch");
     Route::post('/updateForm/{id}',[PitchController::class,"updateForm"])->name("updateform");
@@ -37,7 +41,9 @@ Route::prefix("post")->group(function(){
     Route::get("/", [PostController::class, "showPosts" ])->name("post.show");
     Route::post('/delete/{id}', [PostController::class, "deletePosts"])->name("post.delete");
 });
-Route::prefix("login")->group(function () {
+Route::get("review", [ReviewController::class, "showReviews"])->name("showReviews");
+Route::prefix("/")->group(function () {
     Route::get("/",[HomeController::class,"getAdmin"])->name("login.index");
     Route::post('/post', [HomeController::class, "loginAdmin"])->name("loginAdmin");
 });
+Route::get("logout",[HomeController::class, "logout"]);
