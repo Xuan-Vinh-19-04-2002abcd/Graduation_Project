5a1407f108b6eff3277cb2a6639a7ead82bfcb68
tamxin202 <tamdinh793@gmail.com>
2023-03-01 10:03:56 +0700

    fix layout

--- resources/views/pages/customers/index.blade.php
+++ resources/views/pages/customers/index.blade.php
@@ -1,12 +1,12 @@
-@extends('../../layout/main')
+@extends('layout.main')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">    
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
 		rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-		<link rel="stylesheet" href="assets/CSS/shared/iconly.css">
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
-				integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
-				crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-                <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<link rel="stylesheet" href="assets/CSS/shared/iconly.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+		integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+		crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 @section('content')
 <table class="table">
     <thead>
@@ -41,11 +41,6 @@
                 <button class="btn btn-success mb-2">
                     <i class="fas fa-eye icon"></i>
                 </button>
-                <form action="" method="POST">
-                    <button type="submit" class="btn btn-danger">
-                        <i class="fas fa-eye-slash"></i>
-                    </button>
-                </form>
             </td>
         </tr>
         @endforeach
