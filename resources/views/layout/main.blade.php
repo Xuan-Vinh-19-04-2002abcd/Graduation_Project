3b78695280eca1a803f2906c8af49bc4d0f9c69e
tamxin202 <tamdinh793@gmail.com>
2023-03-07 15:42:47 +0700

    feat update file css

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,16 +5,11 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="/assets/CSS/main/app.css">
 		<link rel="stylesheet" href="/assets/CSS/main/app-dark.css">
-	
-
-		<!-- <link rel="stylesheet" href="assets/CSS/main/custom-styles.css"> -->
 		<link rel="shortcut icon" href="/assets/Images/logo/logo.png" type="image/x-icon">
 		<link rel="shortcut icon" href="/assets/Images/logo/logo.png" type="image/png">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
 		rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 		<link rel="stylesheet" href="/assets/CSS/shared/iconly.css">
-
-    
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
 				integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
 				crossorigin="anonymous" referrerpolicy="no-referrer"></script>
